eb8afdcf173789d0baeedb992ab57b888c119ac9
Willy Tarreau <w@1wt.eu>
2021-10-25 23:20:55 +0200

    nolibc/types: move makedev to types.h and make it a macro
    The makedev() man page says it's supposed to be a macro and that some
    OSes have it with the other ones in sys/types.h so it now makes sense
    to move it to types.h as a macro. Let's also define major() and
    minor() that perform the reverse operation.

--- nolibc.h
+++ nolibc.h
@@ -132,12 +132,3 @@ const char *ltoa(long in)
 		*pos-- = '-';
 	return pos + 1;
 }
-
-/* Here come a few helper functions */
-
-/* WARNING, it only deals with the 4096 first majors and 256 first minors */
-static __attribute__((unused))
-dev_t makedev(unsigned int major, unsigned int minor)
-{
-	return ((major & 0xfff) << 8) | (minor & 0xff);
-}
