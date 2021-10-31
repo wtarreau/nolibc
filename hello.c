1646b5ed6539781bd25fd14f57377572c3daa076
Willy Tarreau <w@1wt.eu>
2021-10-31 18:13:23 +0100

    nolibc: update the example to use printf()
    Let's update the example to simplify it. We now have printf() so we can
    get rid of the ugly local functions. Now the code is ~1.1kB long, vs
    ~800 bytes before, but the code is much simpler and way more versatile
    (it only contains main() and no more accessory functions). This remains
    600 times smaller than the glibc static version and even 1.5 times
    smaller than the glibc dynamic version.

--- hello.c
+++ hello.c
@@ -6,52 +6,20 @@
 #include <errno.h>
 #endif
 
-void writestr(int fd, const char *str)
-{
-	write(fd, str, strlen(str));
-}
-
-void writeint(int fd, unsigned long x)
-{
-	writestr(fd, ltoa(x));
-}
-
-/* prints "$hdr: <$val>\n" */
-void print_int(const char *hdr, int val)
-{
-	writestr(1, hdr);
-	writestr(1, ": <");
-	writeint(1, val);
-	writestr(1, ">\n");
-}
-
-/* prints "$hdr: <$val>\n" */
-void print_str(const char *hdr, const char *val)
-{
-	writestr(1, hdr);
-	writestr(1, ": <");
-	writestr(1, val);
-	writestr(1, ">\n");
-}
-
 int main(int argc, char **argv, char **envp)
 {
-	const char *env = envp[0] ? envp[0] : "none";
 	int ret;
 	int err;
 
-	print_int("pid ", getpid());
-	print_int("arg1", argc > 1 ? atol(argv[1]) : 0);
-
-	writestr(1, "msg : <");
-	ret = write(1, "hello", 5);
+	printf("pid : <%d>\n", getpid());
+	printf("arg1: <%ld>\n", argc > 1 ? atol(argv[1]) : 0);
+	ret = printf("msg : <%s>\n", argc > 2 ? argv[2] : "hello");
 	err = errno;
-	writestr(1, ">\n");
 
-	print_int("ret ", ret);
-	print_int("err ", err);
-	print_int("argc", argc);
-	print_str("argv", argv[0]);
-	print_str("envp", env);
+	printf("ret : <%d>\n", ret);
+	printf("err : <%d>\n", err);
+	printf("argc: <%d>\n", argc);
+	printf("argv: <%s>\n", argv[0]);
+	printf("envp: <%s>\n", envp[0] ? envp[0] : "none");
 	return 0;
 }
