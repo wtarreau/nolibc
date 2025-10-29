3e712f70918281f260be1092f9428fd456f8fa58
Thomas Weißschuh <linux@weissschuh.net>
2025-10-29 17:02:53 +0100

    nolibc: prefer the llseek syscall
    Make sure to always use the 64-bit safe system call
    in preparation for 64-bit off_t on 32 bit architectures.
    
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- sys.h
+++ sys.h
@@ -611,9 +611,7 @@ int link(const char *old, const char *new)
 static __attribute__((unused))
 off_t sys_lseek(int fd, off_t offset, int whence)
 {
-#if defined(__NR_lseek)
-	return my_syscall3(__NR_lseek, fd, offset, whence);
-#else
+#if defined(__NR_llseek)
 	__kernel_loff_t loff = 0;
 	off_t result;
 	int ret;
@@ -627,6 +625,8 @@ off_t sys_lseek(int fd, off_t offset, int whence)
 		result = loff;
 
 	return result;
+#else
+	return my_syscall3(__NR_lseek, fd, offset, whence);
 #endif
 }
 
